7859c7c83060f4ac3689e0de64b64151a77bfebd
Matúš Klasovitý <matusklaso@gmail.com>
2020-09-08 13:57:20 +0200

    Spraveny web-development

---
+++ components/OtherTechnologies.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Ďalšie technológie, s ktorými sa kamarátim</h1>
+    <div class="columns">
+      <div class="column"><img src="~/assets/html-5.svg" alt="html" /></div>
+      <div class="column"><img src="~/assets/css.svg" alt="css" /></div>
+      <div class="column"><img src="~/assets/sass.svg" alt="sass" /></div>
+      <div class="column">
+        <img src="~/assets/javascript.svg" alt="javascript" />
+      </div>
+      <div class="column"><img src="~/assets/babel.svg" alt="babel" /></div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <img src="~/assets/bootstrap.svg" alt="bootstrap" />
+      </div>
+      <div class="column"><img src="~/assets/bulma.svg" alt="bulma" /></div>
+      <div class="column"><img src="~/assets/php.svg" alt="php" /></div>
+      <div class="column"><img src="~/assets/npm.svg" alt="npm" /></div>
+      <div class="column"><img src="~/assets/github.svg" alt="github" /></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+// .img-container {
+//   display: flex;
+//   flex-wrap: wrap;
+// }
+.columns {
+  margin-top: 50px;
+  .columns:first-child {
+    margin-bottom: 30px;
+  }
+}
+img {
+  height: 150px;
+}
+</style>
--- pages/webDevelopment.vue
+++ pages/webDevelopment.vue
@@ -1,9 +1,82 @@
 <template>
-  <div></div>
+  <div class="container">
+    <h1>Web development</h1>
+    <div class="columns">
+      <div class="column">
+        <img src="~/assets/web-development.svg" alt="" />
+      </div>
+      <div class="column intro">
+        <p>
+          Sesame snaps apple pie sesame snaps chocolate pastry jelly halvah
+          lollipop croissant. Topping jujubes dragée oat cake sweet cake tootsie
+          roll. Chocolate tart chocolate chocolate bar chupa chups. Jelly
+          gingerbread marzipan jelly-o. Halvah tiramisu fruitcake pudding
+          chocolate chocolate cake sweet roll biscuit. Cupcake pie jelly.
+          Lollipop tart tiramisu pastry cake croissant.
+        </p>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <CardComponent title="Vue.js" :img-path="require('~/assets/vue.svg')" />
+        <CardComponent
+          title="Nuxt.js"
+          :img-path="require('~/assets/nuxt.svg')"
+        />
+        <CardComponent
+          title="Vuetify"
+          :img-path="require('~/assets/vuetify.svg')"
+        />
+        <CardComponent
+          title="Laravel"
+          :img-path="require('~/assets/laravel.svg')"
+        />
+      </div>
+      <div class="column img-column">
+        <img src="~/assets/rocket.svg" alt="" class="rocket" />
+        <img src="~/assets/town.svg" alt="" class="town" />
+        <img
+          src="~/assets/laravel_and_vue.svg"
+          alt=""
+          class="laravel-and-vue"
+        />
+      </div>
+    </div>
+    <OtherTechnologies />
+  </div>
 </template>
 
 <script>
-export default {}
+import CardComponent from '../components/CardComponent.vue'
+import OtherTechnologies from '../components/OtherTechnologies.vue'
+export default {
+  components: {
+    CardComponent,
+    OtherTechnologies,
+  },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.intro {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.img-column {
+  display: flex;
+  flex-direction: column;
+  .rocket {
+    height: 300px;
+  }
+
+  .town {
+    height: 300px;
+    margin: 80px 0;
+  }
+  .laravel-and-vue {
+    height: 350px;
+  }
+}
+</style>
