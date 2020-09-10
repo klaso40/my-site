f408f12295776cd83c7d18ad14c028aaf7d4f9bb
Matúš Klasovitý <matusklaso@gmail.com>
2020-09-10 22:25:45 +0200

    Home page

---
+++ components/InterestCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <img :src="imagePath" alt="" />
+    <h4>{{ title }}</h4>
+    <p>
+      Bonbon ice cream lollipop. Cake dessert chocolate cheesecake marzipan.
+      Chocolate cake toffee chocolate fruitcake jelly sweet sugar plum jelly
+      beans. Jelly-o brownie sweet roll topping. Cupcake biscuit jelly. Gummi
+      bears sesame snaps carrot cake.
+    </p>
+    <div v-if="Object.keys(nuxtLink).length != 0" class="my-link">
+      <nuxt-link :to="nuxtLink"
+        >Zisti viac <img src="~/assets/arrow.svg" alt=""
+      /></nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imagePath: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    nuxtLink: {
+      type: Object,
+      default: () => {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  background: #22324b;
+  padding: 15px 20px;
+  height: 580px;
+  border-radius: 10px;
+  position: relative;
+  img {
+    height: 300px;
+  }
+  p {
+    margin: 15px 0;
+  }
+  .my-link {
+    position: absolute;
+    bottom: 10px;
+    right: 20px;
+    height: fit-content;
+    width: fit-content;
+
+    a {
+      display: flex;
+      align-items: center;
+      color: white;
+    }
+
+    img {
+      height: 20px;
+      right: 20px;
+      width: 50px;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,136 @@
 <template>
-  <div class="container"><h1>DOMOV</h1></div>
+  <div class="container">
+    <div class="center">
+      <div class="photo"></div>
+      <div id="my-title">
+        Ahoj, moje meno je Matúš <br />
+        a som programátor
+        <button class="button">Zisti viac</button>
+      </div>
+      <div class="astronaut-container">
+        <img src="~/assets/astronaut.png" alt="" />
+      </div>
+    </div>
+    <h2>Zaujímam sa o</h2>
+    <div class="columns">
+      <div class="column">
+        <InterestCard
+          title="App development"
+          :nuxt-link="{ name: 'appDevelopment' }"
+          :image-path="require('~/assets/app-development.svg')"
+        />
+      </div>
+      <div class="column">
+        <InterestCard
+          title="Web development"
+          :nuxt-link="{ name: 'webDevelopment' }"
+          :image-path="require('~/assets/web-development.svg')"
+        />
+      </div>
+      <div class="column">
+        <InterestCard
+          title="Dizajn"
+          :image-path="require('~/assets/design.svg')"
+        />
+      </div>
+    </div>
+    <h1>Pracujem hlavne s</h1>
+    <div class="columns images-container">
+      <div class="column">
+        <img src="~/assets/vue.svg" alt="" />
+      </div>
+      <div class="column">
+        <img src="~/assets/vuetify.svg" alt="" />
+      </div>
+      <div class="column">
+        <img src="~/assets/nuxt.svg" alt="" />
+      </div>
+      <div class="column">
+        <img src="~/assets/laravel.svg" alt="" />
+      </div>
+      <div class="column">
+        <img src="~/assets/flutter.svg" alt="" />
+      </div>
+      <div class="column">
+        <img src="~/assets/swift.svg" alt="" />
+      </div>
+    </div>
+    <OtherTechnologies />
+  </div>
 </template>
 
 <script>
-export default {}
+import InterestCard from '../components/InterestCard.vue'
+import OtherTechnologies from '../components/OtherTechnologies.vue'
+export default {
+  components: {
+    InterestCard,
+    OtherTechnologies,
+  },
+}
 </script>
 
-<style>
-/* .container {
+<style lang="scss" scoped>
+.center {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  text-align: center;
-} */
+  //text-align: center;
 
-.title {
+  .photo {
+    height: 330px;
+    width: 330px;
+    border-radius: 50%;
+    background: url('~assets/klaso-bez-pozadia.png');
+    background-size: 100%;
+    background-repeat: no-repeat;
+    border: 8px solid white;
+  }
+
+  #my-title {
+    text-align: start !important;
+    font-family: Montserrat;
+    font-weight: bold;
+    color: white;
+    font-size: 40px;
+
+    //margin: 30px 0;
+    // text-align: center;
+  }
+  .astronaut-container {
+    height: 300px;
+    width: 300px;
+    //border-radius: 50%;
+    background: white;
+    img {
+      height: 150px;
+    }
+  }
+  .button {
+    border-radius: 10px;
+    background: #22324b;
+    border: none;
+    color: #ffffff;
+    width: 150px;
+    margin-top: 20px;
+    display: block;
+  }
+}
+
+h2 {
+  text-align: center;
+  margin-bottom: 50px;
+}
+
+.images-container {
+  img {
+    width: 150px;
+  }
+}
+
+/* .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
@@ -36,5 +150,5 @@ export default {}
 
 .links {
   padding-top: 15px;
-}
+} */
 </style>
